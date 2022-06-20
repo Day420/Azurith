622ce1db3a772ea5cf4cd64b5506e720fd6396d5
day <99032942+Day420@users.noreply.github.com>
2022-06-20 17:14:29 +0200

    Create installer.py

---
+++ installer.py
@@ -0,0 +1,16 @@
+import requests, os, time
+
+if not os.path.isfile("Azurith-x.py"):
+    version = requests.get("https://azurith.day1337.repl.co/azurith/version.txt").text
+    print(f" Installing latest version of Azurith... ({float(version)})")
+    azurithfile = requests.get("https://azurith.day1337.repl.co/azurith.py").text
+    azurith = open("Azurith.py", "w")
+    azurith.write(str(azurithfile))
+    azurith.close()
+    print(" Azurith installed! Launching...")
+    time.sleep(2)
+    os.system("python3 Azurith.py")
+else:
+    print(" Azurith already installed, if you want to update, do it in Azurith.py!")
+
+ 
